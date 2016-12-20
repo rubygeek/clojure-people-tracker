8f2548d0ff96fb7fc9770af9203421d682bd1079
rubygeek <nola@rubygeek.com>
2016-12-20 09:56:33 -0600

    update to 1.9-alpha14, got update working, passing the people to the find-index function

--- src/people_tracker/core.clj
+++ src/people_tracker/core.clj
@@ -14,13 +14,15 @@
 (defn remove-at-index [coll idx] 
   (flatten (conj (subvec coll 0 idx) (subvec coll (inc idx)))))
 
-(defn find-index [name]
-  (.indexOf @people {:name name}))
+(defn find-index
+  ([name]
+    (find-index people name))
+  ([people name]
+   (.indexOf @people {:name name})))
 
 ;; this function has side effects - println
 (defn display-list []
   (doall (map-indexed #(println %1 ":" (:name %2)) @people ))
-  (println "----")
   :done)
 
 ;; repl functions 
@@ -37,19 +39,19 @@
   ([name]
     (remove-person people name))
   ([people name]
-   (let [idx (find-index name)]
+   (let [idx (find-index people name)]
      (if (not= idx -1) 
        (reset! people (remove-at-index @people idx))
        @people))))
 
-(defn update-person [name data]
+(defn update-person
   ([name data]
-    (update-person people name data))
+   (update-person people name data))
   ([people name data]
-    (let [idx (find-index name)
-          person (get @people idx)
-          updated (merge person data)]
-      (if (not= idx -1)
-        (swap! people assoc idx updated)
-        :invalid-update))))
+   (let [idx (find-index people name)
+         person (get @people idx)
+         updated (merge person data)]
+     (if (not= idx -1)
+       (swap! people assoc idx updated)
+       (str "could not find index: " idx)))))
 
--- test/people_tracker/core_test.clj
+++ test/people_tracker/core_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer [deftest testing is]]
             [people-tracker.core :as core]))
  
+(deftest find-index-test
+  (testing
+    (let [people (atom [{:name "bob"}]) ]  
+      (is (= 0 (core/find-index people "bob"))))))
+
 (deftest add-person-test
   (let [people (atom [])]
     (is (= [{:name "bob"}] (core/add-person people {:name "bob"})))))
@@ -12,5 +17,10 @@
         duplicate-call   (core/remove-person people "sal")]
     (is (= [{:name "bob"} {:name "jay"}] after-remove-sal))
     (is (= [{:name "bob"} {:name "jay"}] duplicate-call))))
+
+(deftest update-person-test 
+  (let [people (atom [{:name "ann"}])
+        after-add-ann (core/update-person people "ann" {:phone "123.133.1231"})]
+    (is (= [{:name "ann" :phone "123.133.1231"}] after-add-ann))))
 
 
