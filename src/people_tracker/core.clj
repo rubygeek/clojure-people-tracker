c46ab593b58c649562c95ed15834fcb15e474f18
Nola Stowe <nola@rubygeek.com>
2016-12-20 08:27:55 -0600

    WIP

--- src/people_tracker/core.clj
+++ src/people_tracker/core.clj
@@ -12,14 +12,20 @@
 (defn list-people [] 
   @people)
 
+;; helper functions
+
+(defn remove-at-index [coll idx] 
+  (flatten (conj (subvec coll 0 idx) (subvec coll (inc idx)))))
+
+(defn find-index [name]
+  (.indexOf @people {:name name}))
+
 ;; side effects
+
 (defn display-list-people []
   (doall (map-indexed #(println %1 ":" (:name %2)) @people ))
   (println "----")
   :done )
-
-(defn remove-at-index [coll idx] 
-  (flatten (conj (subvec coll 0 idx) (subvec coll (inc idx)))))
 
 
 (defn add-person
@@ -28,13 +34,24 @@
   ([people person-data]
    (swap! people conj person-data)))
 
+;; consider returning exception when idx not found 
+
 (defn remove-person
   ([name]
     (remove-person people name))
   ([people name]
-   (let [idx (.indexOf @people {:name name})]
+   (let [idx (find-index name)]
      (if (not= idx -1) 
        (reset! people (remove-at-index @people idx))
        @people))))
 
-(defn update-person [name data])
+(defn update-person [name data]
+  ([name data]
+    (update-person people name data))
+  ([people name data]
+    (let [idx (find-index name)
+          person (get @people idx)
+          updated (merge person data)]
+      (if (not= idx -1)
+        (swap! people assoc idx updated)
+        :invalid-update))))
