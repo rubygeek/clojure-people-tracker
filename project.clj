808896981cd3088d46a3d5637e89a70282ef2f44
Nola Stowe <nola@rubygeek.com>
2018-01-04 09:25:30 -0600

    updated version of clojured

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "TODO"
   :license {:name "TODO: Choose a license"
             :url "http://choosealicense.com/"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                ]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
                    :source-paths ["dev"]}})
